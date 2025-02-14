13ec1138657e1ca15973ce80aa234784ad6e45aa
Ratorgis <ryabovm07@mail.ru>
2025-02-14 09:28:01 +0700

    Changing the names of methods, adding the "singly linked list" class

--- data_structure.py
+++ data_structure.py
@@ -98,6 +98,81 @@ class Tree():
         return bypass(self.root)
     def find(self, elem):
         return search(elem, self.root)
-    def printtree(self):
+    def tree_output(self):
         return tree_visualization(self.root, 0)
 
+class Dot():
+    def __init__(self, element) -> None:
+        self.element = element
+        self.next = None
+
+class LinkedList():
+    def __init__(self) -> None:
+        self.head_addres = None
+        self.len = 0
+    def front(self):
+        return self.head_addres.element
+    def push_front(self, element): 
+        e1 = Dot(element)
+        e1.next = self.head_addres
+        self.head_addres = e1
+        self.len += 1
+    def push_back(self, element): 
+        if self.len == 0:
+            self.push_front(element)
+        else:
+            cur = self.head_addres
+            while cur.next != None:
+                cur = cur.next
+            cur.next = Dot(element)
+        self.len += 1
+    def pop_back(self): 
+        if self.len == 1:
+            self.head_addres = None
+        elif self.len == 2:
+            self.head_addres.next = None
+        else:            
+            cur = self.head_addres
+            cur1 = cur.next
+            while cur1.next != None:
+                cur = cur.next
+            cur.next = None
+            self.len -= 1
+    def pop_front(self): 
+        if self.len > 0:
+            cur = self.head_addres.next
+            self.head_addres.next = None
+            self.head_addres = cur
+            self.len -= 1
+        else:
+            print('There are no elements in the list')
+    def insert(self, pos, element): 
+        e1 = Dot(element)
+        if pos > self.len:
+            print("Out of range value for list")
+        else:
+            cur = self.head_addres
+            for i in range(pos-2):
+                cur.next
+            cur1 = cur.next
+            cur.next = e1
+            e1.next = cur1
+        self.len += 1
+    def erase(self, pos):
+        if pos > self.len:
+            print('Out of range value for list')
+        else:
+            cur = self.head_addres
+            for i in range(pos-2):
+                cur.next
+            cur1 = cur.next
+            cur2 = cur1.next
+            cur1.next = None
+            cur.next = cur2
+        self.len -= 1
+    def print_list(self):
+        cur = self.head_addres
+        print(cur.element)
+        while cur.next != None:
+            print(cur.element, end=' ')
+            cur = cur.next
